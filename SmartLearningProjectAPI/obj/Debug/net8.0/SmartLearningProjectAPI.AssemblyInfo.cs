787bfdbb333b9076eaa249787465f80a0bbdac1b
Hishamradi <heshamelsaady329@gmail.com>
2025-11-20 14:55:17 +0200

    Create crud for (course & unit & lessons)

--- SmartLearningProjectAPI/obj/Debug/net8.0/SmartLearningProjectAPI.AssemblyInfo.cs
+++ SmartLearningProjectAPI/obj/Debug/net8.0/SmartLearningProjectAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartLearningProjectAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b41a590a4c534b2be8cf8948cff2eca40c1fc0f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93148a8d3e3a50a146615e597c460d5365baf6aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartLearningProjectAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartLearningProjectAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
